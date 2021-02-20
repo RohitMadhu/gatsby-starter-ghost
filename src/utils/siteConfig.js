2983d8612f05d81741973bd6d1145e0e354115cb
RohitMadhu <RohitMadhu@users.noreply.github.com>
2021-02-20 17:11:53 -0500

    config the url correctly

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `optimistic-mccarthy-89f51d.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://optimistic-mccarthy-89f51d.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
